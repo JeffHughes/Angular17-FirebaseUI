2992a64a46a722fc0b202f54532f98b3442f06c4
Jeff Hughes <me@jeffhughes.com>
2024-03-21 16:24:19 -0500

    init working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { environment } from '../environments/environment';
+import { firebaseUiAuthConfig } from './firebaseUIAuthConfig';
+import { FirebaseUIModule } from 'firebaseui-angular';
+
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule, USE_EMULATOR as USE_AUTH_EMULATOR } from "@angular/fire/compat/auth";
 
 @NgModule({
   declarations: [
@@ -10,9 +20,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseOptions)),
+    provideAuth(() => getAuth()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    AngularFireModule.initializeApp(environment.firebaseOptions),
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
-  providers: [],
+  providers: [
+   // { provide: USE_AUTH_EMULATOR, useValue: !environment.production ? ['localhost', 9099] : undefined },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = { 
+    production: false,
+    firebaseOptions: {
+        "projectId": "relivetheshowsite",
+        "appId": "1:628040606676:web:8028c17d97203eb3e76c41",
+        "storageBucket": "relivetheshowsite.appspot.com",
+        "locationId": "us-central",
+        "apiKey": "AIzaSyBJZozmvj1vSnK_37mYj9rT9IzgFJ3rSJU",
+        "authDomain": "relivetheshowsite.firebaseapp.com",
+        "messagingSenderId": "628040606676",
+        "measurementId": "G-WJ2WS1W74H"
+    } 
+};
